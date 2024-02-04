e254afca50b92b0d6bdda4d7f28fa8ebba8136b9
Gary Gause <garygause@gmail.com>
2024-02-03 23:58:36 -0800

    add mongodb, contact and portfolio api routes

--- src/app/api/portfolio/route.ts
+++ src/app/api/portfolio/route.ts
@@ -1,31 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { connectDB } from '@/app/lib/mongodb';
-import Contact from '@/app/models/contact';
 import mongoose from 'mongoose';
 
+import { connectDB } from '../../lib/mongodb';
+import Portfolio from '../../models/portfolio';
+
 export async function POST(req: NextRequest) {
-  const { fullName, email, message } = await req.json();
+  const { title, description } = await req.json();
   try {
-    console.log('connecting to db');
-
     await connectDB();
-    console.log('connecting to db');
-    console.log('creating contact');
-    await Contact.create({ fullName, email, message });
+    await Portfolio.create({ title, description });
 
-    return NextResponse.json({ msg: ['Message sent.'], success: true });
+    return NextResponse.json({ msg: ['Portfolio saved.'], success: true });
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
       let errorList = [];
       for (let e in error.errors) {
         console.log(e);
-        errorList.push(error.errors[e].message);
+        //errorList.push(error.errors[e].message);
       }
       return NextResponse.json({ msg: errorList, success: false });
     } else {
       return NextResponse.json({
-        msg: 'Unable to send message.',
+        msg: 'Unable to save Portfolio.',
         success: false,
       });
     }
---
+++ src/components/portfolio-form/portfolio-form.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import React, { useState } from 'react';
+
+function PortfolioForm() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/portfolio', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
+    });
+    const { msg, success } = await res.json();
+    setError(msg);
+    setSuccess(success);
+    if (success) {
+      setTitle('');
+      setDescription('');
+    }
+  };
+
+  return (
+    <>
+      <form
+        className="py-4 mt-4 flex flex-col gap-5"
+        onSubmit={handleFormSubmit}
+      >
+        <div>
+          <label htmlFor="title">
+            Title: <span className="text-palette-red-50">*</span>
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            placeholder="Title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">
+            Description: <span className="text-palette-red-50">*</span>
+          </label>
+          <textarea
+            className="h-32"
+            id="description"
+            value={description}
+            placeholder="Description"
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+        <button
+          className="bg-palette-red font-bold py-3 text-white"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+      <div className="flex flex-col">
+        {error &&
+          error.map((e) => (
+            <div
+              className={`px-5 py-2 mt-5 ${
+                success ? 'text-white' : 'text-palette-red-700'
+              }`}
+              key={e}
+            >
+              {e}
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
+
+export default PortfolioForm;
