f9702fa293de634c615596eb376f757ba42b3239
Gary Gause <garygause@gmail.com>
2024-02-05 15:54:58 -0800

    add opengraph meta

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,6 +13,23 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'gause.dev',
   description: 'Freelance software development services offered by Gary Gause.',
+  openGraph: {
+    title: 'gause.dev',
+    description:
+      'Freelance software development services offered by Gary Gause.',
+    url: 'https://gause.dev/',
+    siteName: 'gause.dev',
+    type: 'website',
+    images: [
+      {
+        url: 'https://gause.dev/site-preview.png',
+        secureUrl: 'https://gause.dev/site-preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'Preview image for gause.dev',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
