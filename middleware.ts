f2419e552c4a959377181b724957ccc36315c8d9
Gary Gause <garygause@gmail.com>
2024-02-16 21:14:08 -0800

    add sharp package, fix blog page error

--- middleware.ts
+++ middleware.ts
@@ -7,3 +7,21 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
+// import { withAuth } from 'next-auth/middleware';
+
+// // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
+// export default withAuth({
+//   callbacks: {
+//     authorized({ req, token }) {
+//       // `/admin` requires admin role
+//       if (req.nextUrl.pathname === '/admin') {
+//         return token?.userRole === 'admin';
+//       }
+//       // `/me` only requires the user to be logged in
+//       return !!token;
+//     },
+//   },
+// });
+
+// export const config = { matcher: ['/admin', '/me'] };
