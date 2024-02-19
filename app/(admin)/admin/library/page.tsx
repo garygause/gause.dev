5614c524681ca3b3a0d5490a8157865546f77de7
Gary Gause <garygause@gmail.com>
2024-02-19 12:13:55 -0800

    remove auth logging, remove test

--- app/(admin)/admin/library/page.tsx
+++ app/(admin)/admin/library/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import LibraryImagesTable from '@ui/admin/library-images';
-import { getLibraryImages } from '@/app/lib/mongodb';
+//import { getLibraryImages } from '@/app/lib/mongodb';
+import { getLibraryImages } from '@/app/lib/api-client';
 
 export default async function LibraryImagePage() {
-  //const { msg, success, data } = await getLibraryImages();
-  const data = await getLibraryImages();
+  const { msg, success, data } = await getLibraryImages();
+  //const data = await getLibraryImages();
   if (!data) {
     return <div>Loading...</div>;
   }
