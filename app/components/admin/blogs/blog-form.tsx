0824e116bf253de33ce04a7e9b647e845227af28
Gary Gause <garygause@gmail.com>
2024-07-17 12:19:32 -0700

    update libs

--- app/components/admin/blogs/blog-form.tsx
+++ app/components/admin/blogs/blog-form.tsx
@@ -12,12 +12,9 @@ export default async function BlogForm({ blog }: { blog: Blog | null }) {
   const id = blog?.id || '';
 
   return (
-    <>
-      <form
-        className="py-4 mt-4 flex flex-col gap-5"
-        action={saveBlogForm.bind(null, id)}
-      >
-        <div>
+    <form action={saveBlogForm.bind(null, id)}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        <div className="mb-4">
           <label htmlFor="name">
             Name: <span className="text-palette-red-500">*</span>
           </label>
@@ -27,7 +24,7 @@ export default async function BlogForm({ blog }: { blog: Blog | null }) {
             name="name"
             defaultValue={blog?.name}
             placeholder="Name"
-            className="dark:bg-palette-white dark:text-palette-black"
+            className="w-full rounded-md border border-gray-200 py-2 pl-4 outline-2 placeholder:text-gray-500"
           />
         </div>
         <MetaFields
@@ -35,21 +32,22 @@ export default async function BlogForm({ blog }: { blog: Blog | null }) {
           url={blog?.metaUrl}
           keywords={blog?.metaKeywords}
           description={blog?.metaDescription}
+          isVisible={true}
         />
         <Status
           statuses={Object.values(BLOGSTATUS)}
           selectedStatus={blog?.status || BLOGSTATUS.active}
         />
-        <div className="mt-6 mr-6 flex flex-row justify-end gap-4">
-          <Link
-            href={PATHS.home}
-            className="flex h-10 items-center rounded-lg bg-gray-100 px-4 font-medium text-gray-600 transition-colors hover:bg-gray-200"
-          >
-            Cancel
-          </Link>
-          <Button type="submit">Save Blog</Button>
-        </div>
-      </form>
-    </>
+      </div>
+      <div className="mt-6 mr-6 flex flex-row justify-end gap-4">
+        <Link
+          href={PATHS.home}
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Save Blog</Button>
+      </div>
+    </form>
   );
 }
