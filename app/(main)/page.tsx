ecaa104d71b6e5906676f3cc7c88fb1da03851aa
Gary Gause <garygause@gmail.com>
2024-07-21 09:40:09 -0700

    force home dynamic

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,12 +5,13 @@ import { BlogTeaser } from '@ui/blog';
 
 import { getJadeClient } from '@lib/client';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const client = getJadeClient();
   const { data: posts, meta } = await client.blogs.searchPosts(
     'limit=3&status=published'
   );
-  console.log('SEARCH POSTS: ', meta);
   return (
     <>
       <Hero />
